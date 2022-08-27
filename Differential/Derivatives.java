aa15cdd6ec9031d7761639330ac23c5e313ae822
shawncogan <112126228+shawncogan@users.noreply.github.com>
2022-08-27 17:38:09 -0400

    Update Derivatives.java

--- Differential/Derivatives.java
+++ Differential/Derivatives.java
@@ -1,14 +1,33 @@
 /*Author: Shawn Cogan 
 8/26/22
 */
+
 package Differential;
+//import java.util.Scanner;
 
 
 
 public class Derivatives {
+
+public static void powerFunction(String function, String variable)
+{
+  
+   System.out.println("The derivative of function " + function + " with respect to " + variable + " is");
+
+   
+}
+
+
 public static void main(String[] args)
  {
-    System.out.println("Math is Cool" + Math.PI);
+    powerFunction("x^5", "x");
+    /* 
+   
+
+ 
+    System.out.println 
+    scan.nextLine()
+    System.out.println("Math is Cool" + Math.PI); */
 
 }
     }
