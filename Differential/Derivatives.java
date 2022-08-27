2b636637701f44396ae5e5bc73b1df12aaa60fbd
shawncogan <112126228+shawncogan@users.noreply.github.com>
2022-08-26 22:51:15 -0400

    Update Derivatives.java

--- Differential/Derivatives.java
+++ Differential/Derivatives.java
@@ -1,8 +1,14 @@
+/*Author: Shawn Cogan 
+8/26/22
+*/
 package Differential;
-/*Author: Shawn Cogan */
-    public class Derivatives {
 
-public static void main(String[] args) {
-    System.out.println("Math is Cool");
+
+
+public class Derivatives {
+public static void main(String[] args)
+ {
+    System.out.println("Math is Cool" + Math.PI);
+
 }
     }
