c6541004f3f0527a907041d0029ff0e969df7bec
shawncogan <112126228+shawncogan@users.noreply.github.com>
2022-08-27 23:46:22 -0400

    Update Derivatives.java

--- Differential/Derivatives.java
+++ Differential/Derivatives.java
@@ -5,18 +5,26 @@
 package Differential;
 //import java.util.Scanner;
 
-
+import java.util.Arrays;
 
 public class Derivatives {
 
 public static void powerFunction(String functionIn, String variable)
 {
+   String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+   System.out.println(Arrays.stream(num).anyMatch("5"::equals));
    String function = functionIn.replace(variable,"x");
    String pow = function.substring((function.indexOf("x^")+2));
+   if (Double.parseDouble(pow)>0){
    double power = Double.parseDouble(pow);
-   System.out.println("" + (power +1) );
+   System.out.println(""+ power);
+   }
+   //double dpow = power - 1;
 
-   System.out.println("The derivative of function " + functionIn + " with respect to " + variable + " is " + power);
+
+   
+
+  // System.out.println("The derivative of function " + functionIn + " with respect to " + variable + " is " + power);
 
    
 }
